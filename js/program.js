9afa0a1ecf5fb511c99d216b5921ad9d691eea30
Eric Peach <eric.peach@gmail.com>
2018-01-17 03:14:43 -0800

    Add a harder puzzle that's enabled by default.

--- js/program.js
+++ js/program.js
@@ -146,7 +146,7 @@ sudoku.program = function() {
     // #############
     // ############# MOCKED DATA!
     // #############
-    // userInputData = _mockout_data();
+    userInputData = _mockout_data();
 
     // Attempt to create the solver with the data.
 
@@ -234,13 +234,13 @@ sudoku.program = function() {
 }();
 
 function _mockout_data() {
-  return [ [ 2, null, null, null, null, null, null, null, null, ],
-      [ null, 6, null, 7, 8, null, 9, null, null, ],
-      [ null, 8, null, null, null, 9, 4, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ],
-      [ null, null, null, null, null, null, null, null, null, ], ];
+  return [ [ 6, null, null, 9, 5, null, 3, null, null, ],
+      [ null, 1, null, null, null, 4, null, null, null, ],
+      [ 4, null, null, null, null, null, 1, null, null, ],
+      [ null, 3, 4, null, null, 6, null, 1, null, ],
+      [ 2, null, null, null, null, null, null, null, 5, ],
+      [ null, 6, null, 4, null, null, 7, 8, null, ],
+      [ null, null, 2, null, null, null, null, null, 8, ],
+      [ null, null, null, 2, null, null, null, 7, null, ],
+      [ null, null, 9, null, 6, 3, null, null, 1, ], ];
 }
